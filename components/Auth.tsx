86ccdd77984a112ea40e4320b3a1febb3034871d
RehanSportskeeda <93132708+RehanSportskeeda@users.noreply.github.com>
2025-09-09 22:42:20 +0530

    Make error icon smaller

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -310,7 +310,7 @@ const Auth: React.FC<AuthProps> = ({ onClose }) => {
               borderRadius: '6px'
             }}>
               <div style={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }}>
-                <svg className="w-4 h-4 text-red-500 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                <svg style={{ width: '12px', height: '12px', color: '#ef4444', marginTop: '2px', flexShrink: 0 }} fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
                 </svg>
                 <span style={{ wordBreak: 'break-words' }}>{error}</span>
