962aa0f90b601ed0ba82c55e37ff124efaef3835
RehanSportskeeda <93132708+RehanSportskeeda@users.noreply.github.com>
2025-09-09 19:44:20 +0530

    Remove all icons from InfoBar to save space

--- components/InfoBar.tsx
+++ components/InfoBar.tsx
@@ -7,21 +7,9 @@ interface InfoBarProps {
 }
 
 const InfoBar: React.FC<InfoBarProps> = ({ message, type }) => {
-  const getIcon = () => {
-    switch (type) {
-      case 'hint':
-        return '💡'
-      case 'success':
-        return '🎉'
-      default:
-        return '💡'
-    }
-  }
-
   return (
     <div className={`${styles.infoBar} ${styles[type]}`}>
       <div className={styles.infoContent}>
-        {getIcon() && <span className={styles.infoIcon}>{getIcon()}</span>}
         <span className={styles.infoText}>{message}</span>
       </div>
     </div>
