beca308187755c687fdcd0262424ab5374774d6e
RehanSportskeeda <93132708+RehanSportskeeda@users.noreply.github.com>
2025-09-12 13:34:28 +0530

    Updated useGameLogic.ts

--- hooks/useGameLogic.ts
+++ hooks/useGameLogic.ts
@@ -122,7 +122,13 @@ export function useGameLogic(initialPuzzleData: PuzzleData) {
     for (let col = 0; col < puzzleData.gridSize; col++) {
       let colFootballs = 0
       for (let row = 0; row < puzzleData.gridSize; row++) {
-        if (board[row][col] === '🏈') colFootballs++
+        if (board[row][col] === '
+        )
+      }
+    }
+  }
+  )
+}🏈') colFootballs++
       }
       if (colFootballs !== 1) return false
     }
